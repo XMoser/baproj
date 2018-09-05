e83b76fce9cbfed2438fca90c3685e46b7e55431
Xavier Moser <xavier.moser@epfl.ch>
2018-09-05 15:40:35 +0200

    Added node count to trie condition

--- lpm/lpm_trie/lpm_trie.h
+++ lpm/lpm_trie/lpm_trie.h
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stddef.h>
 
+#define SIZE_MAX 65535
 #define LPM_TREE_NODE_FLAG_IM	1
 #define LPM_DATA_SIZE 		4
 #define LPM_PLEN_MAX		32
@@ -16,15 +17,15 @@ struct lpm_trie_node {
 	uint32_t prefixlen;
 	uint32_t flags;
 	uint8_t data[LPM_DATA_SIZE];
-	int value;
+	int *value;
 };
 
 struct lpm_trie {
 	struct lpm_trie_node *root;
 	size_t n_entries;
     size_t max_entries;
-	struct lpm_trie_node *node_mem_blocks;
-	struct lpm_trie_node **node_ptr_stack;
+	void *node_mem_blocks;
+	uintptr_t *node_ptr_stack;
 	size_t next_ptr_index;
 };
 
@@ -36,8 +37,7 @@ struct lpm_trie_key {
 //@ #include <listex.gh>
 
 /*@
-	inductive option<t> = some(t) | nil;
-	inductive lpm_node = node(lpm_node, list<int>, option<int>, lpm_node) | nil;
+	inductive lpm_node = node(lpm_node, list<int>, option<int>, lpm_node) | empty;
 	inductive lpm_trie = trie(lpm_node, int, int);
 
 	predicate lpm_trie_p(struct lpm_trie *trie, lpm_trie t);
@@ -46,15 +46,8 @@ struct lpm_trie_key {
 
 	fixpoint bool is_empty(option<t> opt){
 		switch(opt) {
+			case none: return true;
 			case some(v): return false;
-			case nil: return true;
-		}
-	}
-
-	fixpoint t get<t>(option<t> opt){
-		switch(opt) {
-			case nil:
-			case some(v): return v;
 		}
 	}
 
@@ -72,7 +65,7 @@ struct lpm_trie_key {
 
 	fixpoint int match_length(lpm_node node, list<int> p){
 		switch(node) {
-			case nil: return 0;
+			case empty: return 0;
 			case node(lc, np, v, rc):
 				return match_length_aux(np, p, 0);
 			case im_node(lc, np, rc):
@@ -102,10 +95,10 @@ struct lpm_trie_key {
 			return true;
 		} else {
 			switch(node) {
-				case nil: return false;
+				case empty: return false;
 				case node(n_lc, np, nv, n_rc):
 					switch(root) {
-						case nil: return false;
+						case empty: return false;
 						case node(r_lc, rp, rv, r_rc):
 							if(match_length(root, np) < length(rp)){
 								return false;
@@ -127,18 +120,14 @@ struct lpm_trie_key {
 
 	fixpoint bool is_im_node(lpm_node node){
 		switch(node) {
-			case nil: return false;
-			case node(lc, p, v, rc):
-				switch(v) {
-					case nil: return true;
-					case some(val): return false;
-				}
+			case empty: return false;
+			case node(lc, p, v, rc): return is_empty(v);
 		}
 	}
 
 	fixpoint bool is_right_child(lpm_node par, lpm_node node){
 		switch(par) {
-			case nil: return false;
+			case empty: return false;
 			case node(p_lc, pp, pv, p_rc):
 				return node_equals(node, p_rc);
 		}
@@ -146,7 +135,7 @@ struct lpm_trie_key {
 
 	fixpoint bool is_left_child(lpm_node par, lpm_node node){
 		switch(par) {
-			case nil: return false;
+			case empty: return false;
 			case node(p_lc, pp, pv, p_rc):
 				return node_equals(node, p_lc);
 		}
@@ -161,10 +150,10 @@ struct lpm_trie_key {
 
 	fixpoint bool same_prefix(lpm_node n1, lpm_node n2){
 		switch(n1) {
-			case nil: return false;
+			case empty: return false;
 			case node(n1_lc, n1_p, n1_v, n1_rc):
 				switch(n2) {
-					case nil: return false;
+					case empty: return false;
 					case node(n2_lc, n2_p, n2_v, n2_rc):
 						return n1_p == n2_p;
 				}
@@ -174,28 +163,31 @@ struct lpm_trie_key {
 	fixpoint bool contains_prefix(lpm_trie trie, list<int> p){
 		switch(trie) {
 			case trie(root, n, m):
-				lpm_node p_node = node(nil, p, nil, nil);
+				lpm_node p_node = node(empty, p, none, empty);
 				return node_search(root, p_node, same_prefix);
 		}
 	}
 
 	fixpoint bool trie_condition(lpm_trie trie){
 		switch(trie) {
-			case trie(nil, 0, m): return true;
 			case trie(r, n, m):
-				return trie_cond_nodes(r);
+				if(n == 0){
+					return node_equals(r, empty);
+				} else {
+					return trie_cond_nodes(r) && node_count(trie) == n;
+				}
 		}
 	}
 
 	fixpoint bool has_two_children(lpm_node node){
 		swicth(node) {
-			case nil: return false;
+			case empty: return false;
 			case node(lc, p, v, rc):
 				switch(lc) {
-					case nil: return false;
+					case empty: return false;
 					case node(llc, lp, lv, lrc):
 						switch(rc) {
-							case nil: return false;
+							case empty: return false;
 							case node(rlc, rp, rv, rrc):
 								return true;
 						}
@@ -205,7 +197,7 @@ struct lpm_trie_key {
 
 	fixpoint bool trie_cond_nodes(lpm_node node){
 		switch(node) {
-			case nil: return true;
+			case empty: return true;
 			case node(lc, p, v, rc):
 				if(is_im_node(node)){
 					return has_two_children(node) && valid_child(lc, p, 0) &&
@@ -220,7 +212,7 @@ struct lpm_trie_key {
 
 	fixpoint bool valid_child(lpm_node child, list<int> p_pref, int diff){
 		switch(child) {
-			case nil: return true;
+			case empty: return true;
 			case node(c_lc, cp, cv, c_rc):
 				if(match_length(child, p_pref) < length(p_pref)){
 					return false;
@@ -232,10 +224,39 @@ struct lpm_trie_key {
 		}
 	}
 
+	fixpoint int node_count(lpm_trie trie){
+		switch(trie) {
+			case trie(r, n, m):
+				return node_count_aux(r, 0);
+		}
+	}
+
+	fixpoint int node_count_aux(lpm_node root, int acc){
+		switch(root) {
+			case empty:
+			case node(lc, p, v, rc):
+				switch(lc) {
+					case empty:
+						switch(rc) {
+							case empty: return acc;
+							case node(r_lc, rp, rv, r_rc):
+								return node_count_aux(rc, acc+1);
+						}
+					case node(l_lc, lp, lv, l_lc):
+						switch(rc) {
+							case empty: return node_count_aux(lc, acc+1);
+							case node(l_lc, lp, lv, l_rc):
+								return node_count_aux(lc, acc+1) +
+								       node_count_aux(rc, acc + node_count_aux(lc, acc+1));
+						}
+				}
+		}
+	}
+
 	fixpoint lpm_trie lpm_trie_update(lpm_trie trie, list<int> p, option<int> v){
 		switch(trie){
 			case trie(root, n, m):
-				lpm_node new_node = node(nil, p, v, nil);
+				lpm_node new_node = node(empty, p, v, empty);
 				return trie(lpm_trie_update_nodes(root, new_node),
 			                lpm_trie_update_size(trie, p), m);
 		}
@@ -254,10 +275,10 @@ struct lpm_trie_key {
 
 	fixpoint lpm_node lpm_trie_update_nodes(lpm_node root, lpm_node new){
 		switch(root) {
-			case nil: return new;
+			case empty: return new;
 			case node(r_lc, rp, rv, r_rc):
 				switch(new) {
-					case nil:
+					case empty:
 					case node(n_lc, np, nv, n_rc):
 						if(match_length(root, np) == length(rp)
 						   && length(rp) == length(np)){
@@ -282,9 +303,9 @@ struct lpm_trie_key {
 						} else if(match_length(root, np) < length(rp) &&
 						          length(rp) == length(np)){
 							if(nth(length(np)-1, np) == 0){
-								return node(new, make_im_prefix(np, rp), nil, root);
+								return node(new, make_im_prefix(np, rp), none, root);
 							} else if(nth(length(np)-1, np) == 1){
-								return node(root, make_im_prefix(np, rp), nil, new);
+								return node(root, make_im_prefix(np, rp), none, new);
 							}
 						}
 
@@ -311,23 +332,24 @@ struct lpm_trie_key {
 	fixpoint lpm_trie lpm_trie_delete(lpm_trie trie, list<int> p){
 		switch(trie) {
 			case lpm_trie(root, n, m):
-				return trie(lpm_trie_delete_nodes(nil, nil, root, p), n-1, m);
+				return trie(lpm_trie_delete_nodes(node(root, nil, none, empty), p),
+				            n-1, m);
 		}
 	}
 
 	fixpoint lpm_node remove_node(lpm_node par, lpm_node rem){
 		switch(rem) {
-			case nil:
+			case empty:
 			case node(rem_lc, remp, remv, rem_rc){
 				switch(rem_lc){
-					case nil:
+					case empty:
 						switch(rem_rc) {
-							case nil:
+							case empty:
 								//no children
 								if(is_im_node(par)){
 									//check for sibling, return it if it exists
 									switch(par) {
-										case nil:
+										case empty:
 										case node(p_lc, pp, pv, p_rc):
 											if(is_left_child(par, rem)){
 												return p_rc;
@@ -338,19 +360,19 @@ struct lpm_trie_key {
 								} else {
 									//remove rem
 									switch(par) {
-										case nil:
+										case empty:
 										case node(p_lc, pp, pv, p_rc):
 											if(is_left_child(par, rem)){
-												return node(nil, pp, pv, p_rc);
+												return node(empty, pp, pv, p_rc);
 											} else if(is_right_child(par, rem)){
-												return node(p_lc, pp, pv, nil);
+												return node(p_lc, pp, pv, empty);
 											}
 									}
 								}
 							case node(rem_rlc, rem_rp, rem_rv, rem_rrc):
 								//one child, to the right
 								switch(par) {
-									case nil:
+									case empty:
 									case node(p_lc, pp, pv, p_rc):
 										//replace rem with rem_rc
 										if(is_left_child(par, rem)){
@@ -362,10 +384,10 @@ struct lpm_trie_key {
 						}
 					case node(rem_llc, rem_lp, rem_lv, rem_lrc):
 						switch(rem_rc) {
-							case nil:
+							case empty:
 								//one child, to the left
 								switch(par) {
-									case nil:
+									case empty:
 									case node(p_lc, pp, pv, p_rc):
 										//replace rem with rem_lc
 										if(is_left_child(par, rem)){
@@ -377,9 +399,9 @@ struct lpm_trie_key {
 							case node(rem_rlc, rem_rp, rem_rv, rem_rrc):
 								//two children -> mark rem as intermediary node
 								if(is_left_child(par, rem)){
-									return node(node(rem_lc, remp, nil, rem_rc), pp, pv, p_rc);
+									return node(node(rem_lc, remp, none, rem_rc), pp, pv, p_rc);
 								} else if(is_right_child(par, rem)){
-									return node(p_lc, pp, pv, node(rem_lc, remp, nil, rem_rc));
+									return node(p_lc, pp, pv, node(rem_lc, remp, none, rem_rc));
 								}
 						}
 				}
@@ -389,13 +411,13 @@ struct lpm_trie_key {
 
 	fixpoint lpm_node lpm_trie_delete_nodes(lpm_node par, list<int> p){
 		swicth(par) {
-			case nil: return nil;
+			case empty: return empty;
 			case node(p_lc, pp, pv, p_rc):
 				if(match_length(par, p) < length(p) && length(pp) < length(p)){
 					if(nth(length(pp), p) == 0){
 						//check prefix match with left child;
 						switch(p_lc) {
-							case nil: return par;
+							case empty: return par;
 							case node(l_lc, lp, lv, l_rc):
 								if(match_length(p_lc, p) < length(p) && length(lp) < length(p)){
 									return node(lpm_trie_dele_nodes(p_lc, p), pp, pv, p_rc);
@@ -407,7 +429,7 @@ struct lpm_trie_key {
 					} else if(nth(length(pp), p) == 1){
 						//check prefix match with right child;
 						switch(p_rc) {
-							case nil: return par;
+							case empty: return par;
 							case node(r_lc, rp, rv, r_rc):
 								if(match_length(p_rc, p) < length(p) && length(rp) < length(p)){
 									return node(p_lc, pp, pv, lpm_trie_dele_nodes(p_rc, p));
@@ -430,15 +452,15 @@ struct lpm_trie_key {
 
 	fixpoint option<int> trie_lookup_nodes(lpm_node par, lpm_node cur, list<int> p){
 		switch(cur) {
-			case nil:
+			case empty:
 				switch(par) {
-					case nil: return nil;
+					case empty: return none;
 					case node(p_lc, pp, pv, p_rc): return pv;
 				}
 			case node(c_lc, cp, cv, c_rc):
 				if(match_length(cur, p) < length(cp)){
 					switch(par) {
-						case nil: return nil;
+						case empty: return none;
 						case node(p_lc, pp, pv, p_rc): return pv;
 					}
 				}
@@ -461,13 +483,13 @@ struct lpm_trie_key {
 
 struct lpm_trie_node *lpm_trie_node_alloc(struct lpm_trie *trie, int *value);
 /*@ requires value == NULL || *value >= 0; @*/
-/*@ ensures lpm_node_p(result, node(nil, nil,
-                                    value == NULL ? nil : some(*value),
-                                    nil)); @*/
+/*@ ensures lpm_node_p(result, node(empty, nil,
+                                    value == NULL ? none : some(*value),
+                                    empty)); @*/
 
 struct lpm_trie *lpm_trie_alloc(size_t max_entries);
 /*@ requires max_entries > 0; @*/
-/*@ ensures lpm_trie_p(result, trie(nil, 0, max_entries)); @*/
+/*@ ensures lpm_trie_p(result, trie(empty, 0, max_entries)); @*/
 
 void trie_free(struct lpm_trie *trie);
 /*@ requires lpm_trie_p(trie, ?t); @*/
@@ -488,7 +510,7 @@ int *trie_lookup_elem(struct lpm_trie *trie, struct lpm_trie_key *key);
              trie_condition(t); @*/
 /*@ ensures is_empty(trie_lookup(t, p)) ?
 				result == -1 :
-				result == get(trie_lookup(t, p)) &*&
+				result == the(trie_lookup(t, p)) &*&
             trie_condition(t); @*/
 
 int trie_update_elem(struct lpm_trie *trie, struct lpm_trie_key *key, int *value);
